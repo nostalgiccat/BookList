a9ace526098035c0995bcb75f2a2c1de46c00c7c
nostalgiccat <nesdom24@gmail.com>
2024-07-29 20:38:38 -0700

    added props

--- src/index.js
+++ src/index.js
@@ -2,36 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const title = "The Psychology of Money";
+const author = "Morgan Housel";
+const img = "./images/book-1.jpg";
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={img} />
+      <Book author={author} title={title} img={img} />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img src="./images/book-1.jpg" alt="The psychology of money" />
-);
-
-const Title = () => {
-  return <h2>The Psychology of Money</h2>;
-};
-
-const Author = () => <h4>Morgan Housel</h4>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
